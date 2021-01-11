0a1b0107071201e2cc81d98e0170619c783363e5
yongjung95@gmail.com <wjddl1004!!>
2021-01-11 19:58:01 +0900

    배포 자동화 구성 오류수정 테스트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -37,3 +37,5 @@ nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=real \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
+echo "> 테스트"
