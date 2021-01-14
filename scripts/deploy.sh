fa95cb50b77985c6e0749f85740598bcc1e74a91
yongjung <yongjung95@gmail.com>
2021-01-14 13:31:52 +0900

    자동 배포 오류 해결 시도 (scripts/deploy.sh 오타수정)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,25 +5,25 @@ PROJECT_NAME=jung-springbook
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp /home/ec2-user/app/step2/zip/*.jar /home/ec2-user/app/step2/
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
+echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl jung-springbook | grep jar | awk '{print $1}' )
+CURRENT_PID=$(pgrep -fl jung-springbook | grep jar | awk '{print $1}')
 
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다.."
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
+echo "> 새 어플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr /home/ec2-user/app/step2/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
@@ -34,6 +34,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
--Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
--Dspring.profiles.active=real \
-$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > /home/ec2-user/app/step2/nohup.out 2>&1 &
